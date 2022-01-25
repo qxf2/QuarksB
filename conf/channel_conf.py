07b5e2821c17ca9f72b1a3b372a517c0d53b5f1f
Drishya <drishya.tm@qxf2.com>
2022-01-25 12:18:57 +0530

    changed the API conf

--- conf/channel_conf.py
+++ conf/channel_conf.py
@@ -4,4 +4,4 @@ The config file will list channel details for aws configuration
 CHANNEL_ID = "19:1941d15dada14943b5d742f2acdb99aa@thread.skype"
 MESSAGE = "http://google.com"
 
-API_KEY = "M1croservicesFrameworkTester!"
+API_KEY = "<API key>"
