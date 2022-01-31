bed5e65cbecda1b3dca8022904aedcf27beca9ad
Indira Nellutla <indira.nellutla@sciencelogic.com>
2022-01-31 12:19:22 +0530

    added code to verify sending message to skype and check if the queue received

---
+++ conf/skype_conf.py
@@ -0,0 +1,8 @@
+"""
+Specify the conf details needed for skype sender endpoint
+"""
+
+API_KEY = 'YOUR API KEY HERE'
+SKYPE_CHANNEL_ID = 'YOUR CHANNEL ID HERE'
+SKYPE_ENDPOINT = 'https://skype-sender.qxf2.com/send-message'
+SKYPE_MESSAGE = "Hi, This is a test message"
---
+++ conf/sqs_conf.py
@@ -0,0 +1,4 @@
+"""
+The config file will have conf details related to SQS Queues
+"""
+queue_url = 'staging-newsletter-generator'
