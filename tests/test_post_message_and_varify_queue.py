a09e3a210b666dca8da3df41290d23901ca924a5
Mohan Kumar <mohan@qxf2.com>
2022-02-01 13:06:17 +0530

    tests updated

--- tests/test_post_message_and_varify_queue.py
+++ tests/test_post_message_and_varify_queue.py
@@ -16,10 +16,14 @@ def send_message():
     "post request to send a message"
     headers = {'Content-Type': 'application/json'}    
     body = {'API_KEY' : credentials.API_KEY,
-    'msg' : 'https://testexampleUrl.com',
+    'msg' : 'https://test-example.com',
     'channel' : credentials.CHANNEL }
     json_data = json.dumps(body)
     response = requests.post(credentials.BASE_URL, headers = headers, data = json_data)
+    if response.status_code == 200:
+        print("message posted to test channel")
+    else:   
+        print("Failed to post message to test channel")
 
 
 
@@ -27,14 +31,13 @@ def verify_message():
     "verify message from sqs queue"     
     sqs_client =  boto3.client('sqs')
     queue = boto3.resource('sqs').get_queue_by_name(QueueName=credentials.QUEUE_URL)
-    messages = sqs_client.receive_message(QueueUrl=queue.url)
+    messages = sqs_client.receive_message(QueueUrl=queue.url, AttributeNames=['All'], MaxNumberOfMessages=2,MessageAttributeNames=['All'], WaitTimeSeconds=20)
+    
     if 'Messages' in messages:
         for message in messages['Messages']:
             print(message['Body'])
     else:
-        print("No messages")
-
-
+        print("No messages to verify")
 
 
 if __name__ == '__main__':
