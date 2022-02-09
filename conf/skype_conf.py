f8a1466549ce299231524e9196247fd07d5cea2b
akkuldn <akkul.dn@qxf2.com>
2022-02-09 11:08:56 +0530

    Changed the conf file

--- conf/skype_conf.py
+++ conf/skype_conf.py
@@ -2,5 +2,5 @@
 Add your skype configurations here
 """
 skype_sender = 'https://skype-sender.qxf2.com/send-message'
-API_TOKEN = 'Rc7+Kd8Rfc1Rc5R1xc5dxc5Kc6'
+API_TOKEN = ''
 channel_id = '19:1941d15dada14943b5d742f2acdb99aa@thread.skype'
